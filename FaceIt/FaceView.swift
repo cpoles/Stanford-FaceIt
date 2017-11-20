23304d64724666f6fc1d0ab15cdf696561c9414a
cpoles <carlos.poles@griffithuni.edu.au>
2017-11-20 21:41:32 +1000

    Added FaceView as UIView

---
+++ FaceIt/FaceView.swift
@@ -0,0 +1,23 @@
+//
+//  FaceView.swift
+//  FaceIt
+//
+//  Created by Carlos Poles on 20/11/17.
+//  Copyright © 2017 Carlos Poles. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+        
+        let skullRadius = min(bounds.size.width, bounds.size.height) / 2
+        let skullCenter = CGPoint(x: bounds.size.width / 2, y: bounds.size.height / 2)
+        
+        
+    }
+ 
+
+}
