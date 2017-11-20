a2413189ce2aeb5d6345cd442e6c104c264568b0
cpoles <carlos.poles@griffithuni.edu.au>
2017-11-20 21:49:39 +1000

    Added code for drawing circular skull in the centre of the view

--- FaceIt/FaceView.swift
+++ FaceIt/FaceView.swift
@@ -14,7 +14,22 @@ class FaceView: UIView {
         // Drawing code
         
         let skullRadius = min(bounds.size.width, bounds.size.height) / 2
-        let skullCenter = CGPoint(x: bounds.size.width / 2, y: bounds.size.height / 2)
+        
+        
+        // the property center of the view is related to its superview and therefore in a different coordinate system
+        // the same effect can be achieved by using the convert function:
+        //  let skullCenter = convert(center, from: superview)
+        
+        let skullCenter = CGPoint(x: bounds.midX, y: bounds.midY)
+        
+        // Draw a circle
+        let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
+        path.lineWidth = 5.0
+        UIColor.blue.set()
+        path.stroke()
+        
+        
+        
         
         
     }
